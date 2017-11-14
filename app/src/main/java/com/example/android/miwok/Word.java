064a6bd955bd0cf0ef40908fac58a7e3332a1576
nikhil878 <nikhilchaturvedi878@gmail.com>
2017-11-14 21:47:28 +0530

    Just verifying the colab

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,7 +3,7 @@ package com.example.android.miwok;
 /**
  * Created by Abhishek Prasad on 27-Jul-17.
  */
-
+//Just verifying the collabration
 
 public class Word {
     private String mMiwokTranslation;
